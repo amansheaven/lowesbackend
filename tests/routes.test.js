d323508fb0acb7921a26a6c3086c6e8b970d0c54
Aman Singh <amanggnlps@gmail.com>
2020-04-13 11:56:29 +0530

    Fixing workflow tests

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,18 +1,14 @@
-const supertest = require('supertest')
+const request = require('supertest')
 const app = require('../app')
-const request = supertest(app)
 describe('Entrypoint tests...', () => {
   it('Checking boilerplate...', async () => {
-    const res = await request.get('/')
+    const res = await request(app)
+      .get('/')
     expect(res.statusCode).toEqual(200)
   })
-  it('Checking users boilerplate...', async (done) =>{
-      const res = await request.get('/users')
+  it('Checking users boilerplate...', async () =>{
+      const res = await request(app)
+        .get('/users')
       expect(res.statusCode).toEqual(200)
   })
 })
-
-// afterAll((done) => {
-//   app.exit()
-//   done();
-// });
