5ebced2b2a905cbe961b19dabe986e7545c2e984
Aman Singh <amanggnlps@gmail.com>
2020-04-17 11:45:09 +0530

    Added GraphQL

--- routes/users/index.js
+++ routes/users/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/info', function(req, res, next) {
+  res.status(200).send ('UserDB interations here...');
 });
 
 module.exports = router;
