35ca8552f2cb4409c10b13b41591239436eebc5e
Aman Singh <amanggnlps@gmail.com>
2020-04-21 23:45:42 +0530

    Added cors()

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/graphql', (req,res) => {
