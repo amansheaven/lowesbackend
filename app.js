3a904fb9fc7eb578f2bb421c176e3f81121d6c64
Aman Singh <amanggnlps@gmail.com>
2020-04-22 02:02:38 +0530

    fixing favicon

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 const graphQl = require('./schema/index');
@@ -10,6 +9,7 @@ var cors = require('cors');
 var indexRouter = require('./routes');
 
 var app = express();
+var favicon = require('serve-favicon')
 
 // view engine setup
 
