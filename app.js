b4e2cfb653e437f8ecc6b8030623b21b1c7f6da1
Aman Singh <amanggnlps@gmail.com>
2020-04-22 01:28:02 +0530

    removing middlewares

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ var app = express();
 // view engine setup
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(cors());
 
